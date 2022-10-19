cec96fa9043b0b36e13a1766642160b41d3c987a
Thiago Torres <thiagomev@gmail.com>
2022-10-19 09:31:23 -0300

    Add new commands

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
-const { Client, Intents, MessageEmbed } = require("discord.js");
+const { Client, EmbedBuilder } = require("discord.js");
 const client = new Client({
   intents: ["Guilds", "GuildMessages", "GuildVoiceStates", "MessageContent"],
 });
 const { DisTube } = require("distube");
+
+let currentSound = null;
 
 client.DisTube = new DisTube(client, {
   leaveOnStop: false,
@@ -20,23 +22,89 @@ client.on("messageCreate", (message) => {
   if (message.author.bot || !message.guild) return;
   const prefix = "?";
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const argsFormatted = args.shift().toLowerCase();
 
   if (!message.content.toLowerCase().startsWith(prefix)) return;
 
-  if (args.shift().toLowerCase() === "play") {
+  if (argsFormatted === "play" || argsFormatted === "p") {
     client.DisTube.play(message.member.voice.channel, args.join(" "), {
       member: message.member,
       textChannel: message.channel,
       message,
     });
+    const resumeEmbed = new EmbedBuilder()
+      .setColor("#1974D2")
+      .setTitle("Pesquisando a música pedida!")
+      .addFields({
+        name: "Só um segundo, estou procurando sua musica nessa bagunça 😅",
+        value: "Tempo médio de: 3 segundos",
+        inline: true,
+      })
+      .setTimestamp();
+    message.reply({ embeds: [resumeEmbed] });
+    message.react("🔍");
+  }
+
+  if (argsFormatted === "skip") {
+    client.DisTube.skip(message);
     message.react("✅");
   }
-});
 
-client.DisTube.on("playSong", (queue, song) => {
-  queue.textChannel.send(
-    "Tocando agora: \n" + song.name + "\n" + song.thumbnail
-  );
+  if (argsFormatted === "pause") {
+    client.DisTube.pause(message);
+    const pausedEmbed = new EmbedBuilder()
+      .setColor("#CC3333")
+      .setTitle("Musica pausada!")
+      .setThumbnail(currentSound.thumbnail)
+      .addFields({
+        name: currentSound.name,
+        value:
+          `Duração: ${currentSound.formattedDuration} \n` +
+          `Url: ${currentSound.url} \n` +
+          `Views: ${currentSound.views} \n`,
+        inline: true,
+      })
+      .setTimestamp();
+    message.reply({ embeds: [pausedEmbed] });
+    message.react("⏸");
+  }
+
+  if (argsFormatted === "resume") {
+    client.DisTube.resume(message);
+    const resumeEmbed = new EmbedBuilder()
+      .setColor("#0EF907")
+      .setTitle("Continuando")
+      .setThumbnail(currentSound.thumbnail)
+      .addFields({
+        name: currentSound.name,
+        value:
+          `Duração: ${currentSound.formattedDuration} \n` +
+          `Url: ${currentSound.url} \n` +
+          `Views: ${currentSound.views} \n`,
+        inline: true,
+      })
+      .setTimestamp();
+    message.reply({ embeds: [resumeEmbed] });
+    message.react("⏯");
+  }
+
+  client.DisTube.on("playSong", (queue, song) => {
+    currentSound = song;
+    const successEmbed = new EmbedBuilder()
+      .setColor("#0EF907")
+      .setTitle("Tocando agora!")
+      .setThumbnail(song.thumbnail)
+      .addFields({
+        name: song.name,
+        value:
+          `Duração: ${song.formattedDuration} \n` +
+          `Url: ${song.url} \n` +
+          `Views: ${song.views} \n`,
+        inline: true,
+      })
+      .setTimestamp();
+    queue.textChannel.send({ embeds: [successEmbed] });
+  });
 });
 
 client.login(process.env.TOKEN);
