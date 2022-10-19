f0f5b85cbee089113fb5de15d789fa95c67c5986
Thiago Torres <thiagomev@gmail.com>
2022-10-19 03:22:10 -0300

    Fix mesage

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
 require("dotenv").config();
-const discord = require('discord.js');
-const client = new discord.Client({
-  intents: [
-    "Guilds",
-    "GuildMessages",
-    "GuildVoiceStates",
-    "MessageContent"
-  ]
+const { Client, Intents, MessageEmbed } = require("discord.js");
+const client = new Client({
+  intents: ["Guilds", "GuildMessages", "GuildVoiceStates", "MessageContent"],
 });
-const { DisTube } = require('distube');
+const { DisTube } = require("distube");
 
 client.DisTube = new DisTube(client, {
   leaveOnStop: false,
   emitNewSongOnly: true,
   emitAddSongWhenCreatingQueue: false,
-  emitAddListWhenCreatingQueue: false
-})
+  emitAddListWhenCreatingQueue: false,
+});
 
-client.on("ready", client => {
+client.on("ready", (client) => {
   console.log("Bot ready!");
 });
 
-client.on("messageCreate", message => {
-  if(message.author.bot || !message.guild) return;
+client.on("messageCreate", (message) => {
+  if (message.author.bot || !message.guild) return;
   const prefix = "?";
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  
-  if(!message.content.toLowerCase().startsWith(prefix)) return;
-  
-  if(args.shift().toLowerCase() === "play"){
-    client.DisTube.play(message.member.voice.channel, args.join(" "),{
+
+  if (!message.content.toLowerCase().startsWith(prefix)) return;
+
+  if (args.shift().toLowerCase() === "play") {
+    client.DisTube.play(message.member.voice.channel, args.join(" "), {
       member: message.member,
       textChannel: message.channel,
-      message
-    })
+      message,
+    });
+    message.react("✅");
   }
 });
 
 client.DisTube.on("playSong", (queue, song) => {
-  queue.textChannel.send("Tocando agora: " + song.name)
-})
+  queue.textChannel.send(
+    "Tocando agora: \n" + song.name + "\n" + song.thumbnail
+  );
+});
 
 client.login(process.env.TOKEN);
